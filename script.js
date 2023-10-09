3956bccc4eb2fc6e8b5a08fc6eb87b9bea37afdc
cyberbot501 <softwareengineerjoycy@gmail.com>
2023-10-09 15:40:34 +0100

    fucked up codes

--- script.js
+++ script.js
@@ -36,3 +36,20 @@ let changeText = ()=>{
 
 changeText();
 setInterval(changeText,3000)
+
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+
+    for(let i = 0; i < dots ; i++){
+        points += '<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>';
+    }
+
+    elem.innerHTML = points;
+})
