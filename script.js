b165bca8ebf291705747929659827a75cd6ebaac
cyberbot501 <softwareengineerjoycy@gmail.com>
2023-10-25 12:06:45 +0100

    My portfolio completed

--- script.js
+++ script.js
@@ -86,3 +86,23 @@ menuIcon.onclick = ()=>{
     menuIcon.classList.toggle("bx-x");
     navlist.classList.toggle("open");
 }
+
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollButtom = document.querySelectorAll(".scroll-buttom");
+scrollButtom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observe(el));
